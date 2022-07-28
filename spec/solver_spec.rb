697cb573d78e2ef83080bceb0a87ba0988db6f8b
SimbaWANGU <simbawangulu@gmail.com>
2022-07-28 13:24:48 +0300

    update tests

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -17,20 +17,20 @@ describe Solver do
     end 
 
     it "should reverse string when reverse method is called" do 
-      expect(sv.reverse('lion')).to be 'niol'
-      expect(sv.reverse('hello')).to be 'olleh'
-      expect(sv.reverse('phone')).to be 'enohp'
+      expect(sv.reverse('lion')).to eq('noil')
+      expect(sv.reverse('hello')).to eq('olleh')
+      expect(sv.reverse('phone')).to eq('enohp')
     end 
 
     it "should return fizz, buzz, fizzbuzz or string of an integer when fizzbuzz method is called" do 
-      expect(sv.fizzbuzz(3)).to be 'fizz'
-      expect(sv.reverse(9)).to be 'fizz'
-      expect(sv.reverse(5)).to be 'buzz'
-      expect(sv.reverse(10)).to be 'buzz'
-      expect(sv.reverse(15)).to be 'fizzbuzz'
-      expect(sv.reverse(30)).to be 'fizzbuzz'
-      expect(sv.reverse(7)).to be '7'
-      expect(sv.reverse(10)).to be '10'
+      expect(sv.fizzbuzz(3)).to eq('fizz')
+      expect(sv.fizzbuzz(9)).to eq('fizz')
+      expect(sv.fizzbuzz(5)).to eq('buzz')
+      expect(sv.fizzbuzz(10)).to eq('buzz')
+      expect(sv.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(sv.fizzbuzz(30)).to eq('fizzbuzz')
+      expect(sv.fizzbuzz(7)).to eq('7')
+      expect(sv.fizzbuzz(10)).to eq('10')
     end
   end
 end
