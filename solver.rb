0f1e2a2159ff70ff2d9b6a5507a9ace1f99a86ee
camilevahviraki <camilevahviraki@gmail.com>
2022-07-28 14:43:40 +0200

    Edit Solver class to add factorial

--- solver.rb
+++ solver.rb
@@ -1,9 +1,17 @@
+# frozen_string_literal: true
+
+# Handle 3 methods
 class Solver
-  def initialize
-  end
+  def initialize; end
 
   def factorial(int)
-
+    if int.zero?
+      1
+    elsif int.negative?
+      puts 'Math error!'
+    else
+      int * factorial(int - 1)
+    end
   end
 
   def reverse(str)
@@ -11,6 +19,14 @@ class Solver
   end
 
   def fizzbuzz(int)
-
+    if (int % 3).zero? && (int % 5).zero?
+      'fizzbuzz'
+    elsif (int % 3).zero?
+      'fizz'
+    elsif (int % 5).zero?
+      'buzz'
+    else
+      int.to_s
+    end
   end
 end
